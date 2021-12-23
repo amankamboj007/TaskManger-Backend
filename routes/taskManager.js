d0abd6bd1a0efc40fea96158188441e024414411
Aman kamboj <kamboj.aman32@gmail.com>
2021-12-24 01:46:07 +0530

    Added Rotues for Task in the list

--- routes/taskManager.js
+++ routes/taskManager.js
@@ -2,12 +2,15 @@ const router = require('express').Router()
 const taskManagerController = require('../controller/taskManger.controller')
 
 //API's For list
-router.post('/list', taskManagerController.addList)
 router.get('/list', taskManagerController.getList)
+router.post('/list', taskManagerController.addList)
 router.put('/list', taskManagerController.updateList)
 router.delete('/list/:id', taskManagerController.deleteList)
 
 //API's For Task
-router.post('/task',)
+router.get('/list/:listId/tasks', taskManagerController.getTaskForList)
+router.post('/list/:listId/tasks ', taskManagerController.addTaskForList)
+router.patch('/list/:listId/tasks/:taskId',taskManagerController.updateTaskForList)
+router.delete('/list/:listId/tasks/:taskId',taskManagerController.deleteTaskForList)
 
 module.exports = router
